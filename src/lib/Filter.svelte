af4e6f9e6eda90cd79e44e4d649e4d4e1e52c2a8
Michinobu Maeda <michinobumaeda@gmail.com>
2024-10-26 13:31:55 +0900

    bug fix: filter on click

--- src/lib/Filter.svelte
+++ src/lib/Filter.svelte
@@ -14,7 +14,7 @@
     (checked
       ? "pl-1 border-none bg-lightPrimaryContainer dark:bg-darkPrimaryContainer text-lightOnPrimaryContainer dark:text-darkOnPrimaryContainer"
       : "pl-4 border bg-lightSurface dark:bg-darkSurface text-lightOnSurface dark:text-darkOnSurface border-lightOutlineVariant dark:border-darkOutlineVariant")}
-  on:click={ouClick}
+  on:click={() => ouClick(!checked)}
 >
   <span class="flex flex-row">
     {#if checked}
